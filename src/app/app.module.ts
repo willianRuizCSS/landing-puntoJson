59ba0a277f80477e17d0ee788060096271bc6cee
willianRuizCSS <william.ruiz@codeskills.com.co>
2023-05-16 13:49:16 -0500

    se eimplementa home con load children y demas angular 16

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './core/components/header/header.component';
+import { FooterComponent } from './core/components/footer/footer.component';
+import { VideoGridComponent } from './core/components/video-grid/video-grid.component';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    VideoGridComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/core/components/video-grid/video-grid.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-video-grid',
+  templateUrl: './video-grid.component.html',
+  styleUrls: ['./video-grid.component.css']
+})
+export class VideoGridComponent {
+
+}
